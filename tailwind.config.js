1089f7239ca7245f20e2fc83131eb07acdae4800
Parth Patel <patelsparth00@gmail.com>
2025-07-24 19:39:55 -0700

    Fixed tailwind issue

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class', // <–– this gives you manual control
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,9 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // This handles everything - no PostCSS needed
   ],
 })
